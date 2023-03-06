ca6f8713bc20a6b643cade18ca26ce12925dc2b0
Роман Кислов <romankislov@MacBook-Air-Roman.local>
2023-03-06 12:41:53 +0500

    increment 1 client ready

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,13 @@
 package main
 
-func main() {}
+import (
+	"github.com/rkislov/go-metrics.git/handlers"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/update/gauge/", handlers.GaugeHandler)
+	http.HandleFunc("/update/counter/", handlers.CounterHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
---
+++ handlers/gauge.go
@@ -0,0 +1,9 @@
+package handlers
+
+import "net/http"
+
+func GaugeHandler(rw http.ResponseWriter, r *http.Request) {
+	rw.Header().Set("Content-Type", "application/json")
+	rw.WriteHeader(http.StatusOK)
+	rw.Write([]byte(`{"status":"ok"}`))
+}
